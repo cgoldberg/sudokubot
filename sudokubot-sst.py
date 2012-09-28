d8d46ba69cc4bc8fde28a5a6efd17c018fca2b4f
Corey Goldberg <corey@goldb.org>
2012-09-28 18:15:29 -0300

    Update sudokubot-sst.py

--- sudokubot-sst.py
+++ sudokubot-sst.py
@@ -9,18 +9,24 @@ see: http://testutils.org/sst
 """
 
 
+from sudoku import solve
 from sst.actions import *
-import sudoku
 
 
-GAME_LEVEL = 4  # evil
+go_to('http://view.websudoku.com/?level=4')  # evil
 
-go_to('http://view.websudoku.com/?level=%s' % GAME_LEVEL)
-grid_ids = ['f%s%s' % (row, col) for col in xrange(9) for row in xrange(9)]
-grid = [get_element(id=id).get_attribute('value') or '.' for id in grid_ids]
-solution = sudoku.solve(grid)
-solution_grid = (solution[k] for k in sorted(solution))
-for id, val in zip(grid_ids, solution_grid):
-    write_textfield(get_element(id=id), val, check=False, clear=False)
+grid_ids = ['f%s%s' % (row, col) for col in xrange(9)
+    for row in xrange(9)]
+
+grid = [get_element(id=id).get_attribute('value') or '.'
+    for id in grid_ids]
+
+solution = solve(grid)
+
+for (id, val) in zip(grid_ids,
+        (solution[k] for k in sorted(solution))):
+    write_textfield(id, val, check=False, clear=False)
+
 click_element(get_element(name='submit'))
-assert_text_contains(get_element(id='message'), 'Congratulations!')
+
+assert_text_contains('message', 'Congratulations!')
