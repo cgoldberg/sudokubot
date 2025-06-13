300c3035b9072fd33062aa82dc9e41ae7a79dffe
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-06-12 23:08:51 -0400

    Python 3 update!

---
+++ README.md
@@ -0,0 +1,29 @@
+# sudokubot
+
+Browser bot that solves online sudoku puzzles.
+
+Beat the "evil" level on https://websudoku.com every time!
+
+----
+
+### Requirements
+
+- Python 3
+- Selenium WebDriver Python bindings:
+
+```
+    pip install selenium
+```
+
+### Instructions
+
+```
+    python3 sudokubot.py
+```
+
+### Info
+
+- https://websudoku.com
+- https://selenium.dev
+- https://norvig.com/sudoku.html
+- https://en.wikipedia.org/wiki/Sudoku
--- sudokubot.py
+++ sudokubot.py
@@ -1,61 +1,60 @@
-#!/usr/bin/env python
-# Corey Goldberg - 2011
-
-
-"""selenium webdriver browser-bot that solves sudoku puzzles."""
+#!/usr/bin/env python3
+# Corey Goldberg - 2011-2025
 
 
 import time
-import sudoku
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import sudoku
 
 
+NUM_GAMES = 5  # number of games to play
 GAME_LEVEL = 4  # evil
+TIME_BETWEEN_GAMES = 2  # secs
 
 
 def main():
-    bot = SudokuBot(GAME_LEVEL, pause=2.0)
-    while True:
-        print 'game #%d' % (bot.puzzles_solved + 1)
-        bot.play()
-        bot.display_solution()
-        print '----------------'
+    bot = SudokuBot()
+    for i in range(NUM_GAMES):
+        print(f"Playing game #{i + 1}")
+        result = bot.play(level=GAME_LEVEL)
+        print(result)
+        print("----------------")
+        time.sleep(TIME_BETWEEN_GAMES)
+    bot.quit()
+    print("Done")
 
 
-class SudokuBot(object):
-    def __init__(self, game_level=4, pause=0.0):
-        self.game_level = game_level
-        self.pause = pause
-        self._puzzles_solved = 0
-        self._solution = None
-    
-    @property
-    def puzzles_solved(self):
-        return self._puzzles_solved
-        
-    def play(self):
-        browser = webdriver.Firefox()
-        browser.get('http://view.websudoku.com/?level=%s' % self.game_level)
-        grid_ids = ['f%s%s' % (row, col) for col in xrange(9) for row in xrange(9)]       
-        grid = [browser.find_element_by_id(id).get_attribute('value') or '.' for id in grid_ids]
+class SudokuBot:
+
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--disable-infobars")
+        options.add_argument("--window-size=1200,800")
+        self.driver = webdriver.Chrome(options=options)
+
+    def play(self, level=4):
+        self.driver.get(f"https://east.websudoku.com/?level={level}")
+        grid_ids = [f"f{row}{col}" for col in range(9) for row in range(9)]
+        grid = [
+            self.driver.find_element(By.ID, id).get_attribute("value") or "."
+            for id in grid_ids
+        ]
         solution = sudoku.solve(grid)
-        solution_grid = (solution[k] for k in sorted(solution))
-        for id, val in zip(grid_ids, solution_grid):
-            browser.find_element_by_id(id).send_keys(val)
-        browser.find_element_by_name('submit').click()
-        assert 'Congratulations!' in browser.find_element_by_id('message').text
-        if self.pause:
-            time.sleep(self.pause)
-        browser.quit()
-        self._solution = solution
-        self._puzzles_solved += 1
-        
-    def display_solution(self):
-        if self._solution is None:
-            raise Exception('no puzzles solved yet')
-        else:
-            sudoku.display(self._solution)
+        solution_grid = (solution[key] for key in sorted(solution.keys()))
+        for id, value in zip(grid_ids, solution_grid):
+            self.driver.find_element(By.ID, id).send_keys(value)
+        self.driver.find_element(By.NAME, "submit").click()
+        message = self.driver.find_element(By.ID, "message").text
+        if "Congratulations!" in message:
+            return message
+        return "You Lost!"
 
-    
-if __name__ == '__main__':
+    def quit(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
     main()
